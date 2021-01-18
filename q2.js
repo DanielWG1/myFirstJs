dbb1ea389d0042cf68f7313822e330db540232fb
DanielWG1 <Dunchil@gmail.com>
2021-01-18 14:20:15 +0200

    q2 solution

---
+++ q2.js
@@ -0,0 +1,27 @@
+if (x>0 && y>0 && z>0) {
+    alert ('+');
+}
+else if (x>0 && y<0 && z>0) {
+    alert ('-')
+}
+else if (x>0 && y>0 && z<0) {
+    alert ('-');
+}
+else if (x>0 && y<0 && z<0) {
+    alert ('+');
+}
+
+else if (x<0 && y>0 && z>0) {
+    alert ('-')
+}
+else if (x<0 && y>0 && z<0) {
+    alert ('+');
+}
+else if (x<0 && y<0 && z>0) {
+    alert ('+')
+}
+else if (x<0 && y<0 && z<0) {
+    alert ('-')
+}
+
+
