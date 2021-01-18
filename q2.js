351cede59b27aac84bfe5a321e51be36526d7f83
DanielWG1 <Dunchil@gmail.com>
2021-01-18 16:24:11 +0200

    New q2 answer, please check this one.

--- q2.js
+++ q2.js
@@ -1,27 +1,26 @@
-if (x>0 && y>0 && z>0) {
-    alert ('+');
-}
-else if (x>0 && y<0 && z>0) {
-    alert ('-')
-}
-else if (x>0 && y>0 && z<0) {
-    alert ('-');
-}
-else if (x>0 && y<0 && z<0) {
-    alert ('+');
-}
-
-else if (x<0 && y>0 && z>0) {
-    alert ('-')
-}
-else if (x<0 && y>0 && z<0) {
-    alert ('+');
-}
-else if (x<0 && y<0 && z>0) {
-    alert ('+')
-}
-else if (x<0 && y<0 && z<0) {
-    alert ('-')
-}
 
 
+if (x===0 || y===0 || z===0) {
+    alert ('0');
+    
+} else {
+
+    let numNegative=0;
+    if (x<0) {
+        numNegative++;
+    }
+    if (y<0) {
+        numNegative++;
+    }
+
+    if (z<0) {
+        numNegative++;
+
+    }
+
+    if (numNegative % 2===0) {
+        alert ('+');
+    } else {
+        alert ('-');
+    }
+}
