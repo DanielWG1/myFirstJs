d6d15536c9791eb2369d3e30d9af0890c07e7b04
DanielWG1 <Dunchil@gmail.com>
2021-01-18 16:33:04 +0200

    Answer for q4

---
+++ q4.js
@@ -0,0 +1,19 @@
+let inputList=[0, -4, -2, 8];
+let swap;
+    let n = inputList.length-1;
+    let sortedList=inputList;
+    do {
+        swap = false;
+        for (let i=0; i < n; i++)
+        {
+            if (sortedList[i] < sortedList[i+1])
+            {
+               let temp = sortedList[i];
+               sortedList[i] = sortedList[i+1];
+               sortedList[i+1] = temp;
+               swap = true;
+            }
+        }
+        n--;
+    } while (swap);
+    alert (sortedList [0]);
